f329dded124298d8aed502dcfdacd570555bab0a
Lars Trieloff <lars@trieloff.net>
2017-01-06 12:06:50 +0100

    Most prolific commiters in 2016

---
+++ prolific.sql
@@ -0,0 +1,25 @@
+#standardSQL
+SELECT
+  author,
+  PERCENT_RANK() OVER (ORDER BY commits DESC) AS crank,
+  PERCENT_RANK() OVER (ORDER BY repos DESC) AS rrank,
+  repos,
+  commits,
+  SQRT((PERCENT_RANK() OVER (ORDER BY commits DESC)*PERCENT_RANK() OVER (ORDER BY commits DESC))+(PERCENT_RANK() OVER (ORDER BY repos DESC)*PERCENT_RANK() OVER (ORDER BY repos DESC))) AS orank
+FROM (
+  SELECT
+    author.name AS author, 
+    COUNT(DISTINCT REGEXP_REPLACE(repo_name,".*/","")) AS repos, 
+    COUNT(commit) AS commits
+  FROM 
+    `bigquery-public-data.github_repos.commits` AS c
+  LEFT JOIN
+    c.repo_name
+  WHERE
+    EXTRACT(YEAR FROM author.date) = 2016
+  GROUP BY
+    author)
+ORDER BY
+  orank ASC
+LIMIT
+10000
